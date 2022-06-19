ce7dceed1dee43cc48f05a67a5db6027d4db0aa5
u2much4u <andreszxc@gmail.com>
2022-06-19 16:46:26 -0600

    Commit_mejoras_leves

---
+++ app/src/main/java/com/example/podcastgeist/Form_Soporte.kt
@@ -0,0 +1,11 @@
+package com.example.podcastgeist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Form_Soporte : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.form_soporte)
+    }
+}
